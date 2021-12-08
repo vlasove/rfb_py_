1d235363c0809491b243619e14a59941e1ba1c08
EvgenyVl <evgeny_vlasov@yahoo.com>
2021-12-08 13:21:50 +0300

    Лекция 3: целые числа

---
+++ Lec3/example_a.py
@@ -0,0 +1,22 @@
+# Целые числа
+a_int = int(input())
+b_int = int(input())
+
+print(a_int * b_int)
+
+print(f"A value {a_int} and B value {b_int}")
+print("Type of A:", type(a_int))
+print(a_int, b_int)
+
+# Арифметика
+print("a_int + b_int = ", a_int + b_int)
+print("a_int - b_int = ", a_int - b_int)
+print("a_int * b_int = ", a_int * b_int)
+# Нюанс при делении - всегда вещественное число!!!
+print("a_int / b_int = ", a_int / b_int)
+# Классическое целочисленное деление - это //
+print("a_int // b_int = ", a_int // b_int)
+# Взятие остатка от деления
+print("a_int % b_int = ", a_int % b_int)
+# Возведение в степень
+print("a_int ^ b_int = ", a_int ** b_int)
