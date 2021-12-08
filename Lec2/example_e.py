47781bcb4849ee34c1a28b4a6d7cfb613f57c1a2
EvgenyVl <evgeny_vlasov@yahoo.com>
2021-12-08 12:41:00 +0300

    Лекция 2: Считывание и как не надо делать

---
+++ Lec2/example_e.py
@@ -0,0 +1,3 @@
+# Простейший ввод input()
+name = input() # "Alice and Bob"
+print(f"Hello, It's {name}")
---
+++ Lec2/example_f.py
@@ -0,0 +1,7 @@
+# Считывание нескольких значений
+name = input()
+last_name = input()
+age = input()
+
+message = f"Имя: {name} , Фамилия: {last_name} , Возраст: {age} . Студент BPS"
+print(message)
