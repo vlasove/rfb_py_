0d66af9f0f0f7cb7f39afcb50670d76939a4e5b4
EvgenyVl <evgeny_vlasov@yahoo.com>
2021-12-08 13:36:03 +0300

    Лекция 3: гр

---
+++ Lec3/example_b.py
@@ -0,0 +1,13 @@
+# Гадость 1
+first = int(-4/3)
+second = -4 // 3 # При целочисленном делении происходит прижатие к НАИМЕНЬШЕМУ ближайшему числу
+
+print(f"First: {first}")
+print(f"Second: {second}")
+
+
+# Радость 1
+# Замена сператора в print() и замена завершающего символа
+print("Alex", "Fedya", "Bob", sep=" ", end="\n")
+print("Alex", "Fedya", "Bob", end="ENDOFLINE")
+print("Alex", "Fedya", "Bob", sep="@#$#@", end="\n")
