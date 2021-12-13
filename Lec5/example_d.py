ec06d3f8c7ac0a2ac965495759541a3cd07cbfab
EvgenyVl <evgeny_vlasov@yahoo.com>
2021-12-13 11:08:31 +0300

    Добавлен шаблон I

---
+++ Lec5/example_d.py
@@ -0,0 +1,15 @@
+# Шаблон для I
+
+while True:
+    password1 = input()
+    password2 = input()
+    if len(password1) < 8:
+        print("Слишком короткий пароль!")
+    elif ...:
+        ...
+    elif ...:
+        ...
+    else:
+        print("Ну наконец-то!")
+        break
+    
