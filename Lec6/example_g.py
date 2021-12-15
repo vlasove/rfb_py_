f332313c77688f7a42ebdc82b0e05deec3c38257
EvgenyVl <evgeny_vlasov@yahoo.com>
2021-12-15 11:33:01 +0300

    Лекция 6

---
+++ Lec6/example_g.py
@@ -0,0 +1,19 @@
+# Решение задачи S.
+
+from_home_to_park = set()
+while True:
+    message = input()
+    if message == "":
+        break
+    from_home_to_park.add(message)
+
+
+from_park_to_home = set()
+while True:
+    message = input()
+    if message =="":
+        break
+    from_park_to_home.add(message)
+
+result = ....
+print(len(result))
