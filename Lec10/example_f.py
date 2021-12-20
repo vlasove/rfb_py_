df622e23d2a6e8ce0e7364e08867b6103e2b01d3
EvgenyVl <evgeny_vlasov@yahoo.com>
2021-12-20 12:00:17 +0300

    Лекция 10: Решение задачи B

--- Lec10/example_f.py
+++ Lec10/example_f.py
@@ -10,10 +10,16 @@ for _ in range(n):
         last_name = last_name_with_dot_str.split(sep=".")[0] # ["Кузнецов", ""][0]
         queue.append(last_name)
     elif "Я только спросить" in event_message:
-        .........
+        last_name_with_dot_str = event_message.split(sep= " - ")[-1] # ["Я только спросить! Я", "Кузнецов."][-1] -> "Кузнецов."
+        last_name = last_name_with_dot_str.split(sep=".")[0] # ["Кузнецов", ""][0]
+        queue.insert(0, last_name)
     elif "Следующий" in event_message:
         if len(queue) == 0:
             print("В очереди никого нет.")
         else:
             next_person = queue.pop(0) #
             print(f"Заходит {next_person}!")
+
+
+# name = "Петров.".replace(".", "!")
+# name = name[:-1] + "!" # "Петров!"
