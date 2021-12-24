61167cb3feb558a766930971be6c22b44182874d
EvgenyVl <evgeny_vlasov@yahoo.com>
2021-12-24 13:17:46 +0300

    Лекция 15: аннотирование и документирование кода. Ч.1

---
+++ Lec15/example_c.py
@@ -0,0 +1,10 @@
+def add(a :int, b :int) -> int:
+    """
+    Многозначительная документация
+    """
+    return a + b
+
+print(add(2, 30))
+print(add("str", "msg"))
+print(add([1,2], [3,4]))
+print(add((1,2,3), (14,5)))
