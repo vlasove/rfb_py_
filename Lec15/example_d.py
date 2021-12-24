3338f176c81029f409fd5c042bc7a884ee8c9be7
EvgenyVl <evgeny_vlasov@yahoo.com>
2021-12-24 13:20:46 +0300

    Обновление задач по курсу

--- Lec15/example_d.py
+++ Lec15/example_d.py
@@ -1,20 +1,21 @@
-"""
-Пример аннотаций
-"""
+"""Пример аннотаций."""
 from typing import List, Dict
 
-def mutate(a:int, b:int, d : List[int]) -> List[int]:
-    """
-    Никто не знает как работает эта функция.
+
+def mutate(a: int, b: int, d: List[int]) -> List[int]:
+    """Никто не знает как работает эта функция.
+
     Служит для примера аннотиций входных и выходных типов
     """
-    return [a,b] + d
+    return [a, b] + d
 
-def build_map(key:str = "key", value:int=10) -> Dict[str, int] :
+
+def build_map(key: str = "key", value: int = 10) -> Dict[str, int]:
     """
     Строит словарь , где ключ - аргумент key
     а значение - аргумент value
     """
-    return {key : value}
+    return {key: value}
+
 
 build_map()
